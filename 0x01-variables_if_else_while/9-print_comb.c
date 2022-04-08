0978c0ebbd1f8a8da79628ba14ba2a2770465e55
Chinazor Akah <chinazor.akah@gmail.com>
2022-04-08 14:15:22 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: Prints all single digits of base 10
+ * Starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
