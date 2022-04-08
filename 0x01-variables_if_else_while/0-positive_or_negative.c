ba9f12dbd1c588fd784e8d199cea6a0fd636ac7b
Chinazor Akah <chinazor.akah@gmail.com>
2022-04-08 08:19:34 -0700

    Positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
+if (n > 1)
+printf("%d is positive", n);
+else if (n < 0)
 printf("%d is zero/n", n);
 else if
 printf("%d is negative\n", n);
