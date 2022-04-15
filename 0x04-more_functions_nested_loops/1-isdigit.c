c3b47457a5e24426ea011921b551ef72311128c8
Chinazor Akah <chinazor.akah@gmail.com>
2022-04-15 06:22:44 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit -checks if the no is b/n 0 to 9
+ * @c: input no
+ * Return: 1 if its a number (0 to 9),0 in other case.
+ */
+int _isdigit(int c)
+{
+if (c >= 48 & 7 c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
