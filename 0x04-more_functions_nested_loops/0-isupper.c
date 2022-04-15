3e60f05d98cdcfc4856c9613d50f1956362a1c7b
Chinazor Akah <chinazor.akah@gmail.com>
2022-04-15 06:15:16 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,6 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  * _isupper - checks upper case
-=======
- * _isupper - checkes upper case
->>>>>>> 759b736c85fc586e6a3251c1b500090ab9ad2695
  * @c: print int
  * Return: give 1 or 0
  */
@@ -17,11 +13,7 @@ return (1);
 }
 
 else
-<<<<<<< HEAD
 {
-=======
-}
->>>>>>> 759b736c85fc586e6a3251c1b500090ab9ad2695
 return (0);
 }
 
