89f1a14b7e2ef7558315beab1e447f8487eaf9a8
Jenny3094 <98693531+Jenny3094@users.noreply.github.com>
2022-05-16 16:52:07 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,31 @@
-#include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include "3-calc.h"
 /**
- * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
- * @s: The operator passed as argument.
- *
- * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
- */
+* get_op_func - is a function that get an operation
+* @s: operator input
+* Return: function result
+*/
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
-
-	return (ops[i].f);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op)
+{
+if (strcmp(s, ops[i].op) == 0)
+{
+return (ops[i].f);
+}
+i++;
+}
+printf("Error\n");
+exit(99);
 }
