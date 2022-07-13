37a4699e968ff40a439a9682e9992d6234e03599
Chinazor Akah <chinazor.akah@gmail.com>
2022-07-13 15:10:06 -0700

    updated

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-	printf("%d\n", _strlen("Holberton"));
-	return (EXIT_SUCCESS);
+    printf("%d\n", _strlen("Holberton"));
+    return(EXIT_SUCCESS);
 }
